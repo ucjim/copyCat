2d51d795b757cc06b17e6d7d0e739cfe63ae5a76
lauren <lauren@LAURENJANEYBC1C>
2017-07-21 18:35:28 +0100

    Add project files.

---
+++ CopyCat/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CopyCat
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        Stats stats = new Stats();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //Add a random key to the listBox
+
+            letterBox.Items.Add((Keys)random.Next(65, 90));
+            if (letterBox.Items.Count > 7)
+            {
+                letterBox.Items.Clear();
+                letterBox.Items.Add("Game Over!");
+                timer.Stop();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //If the user pressed a key that's in the letterBox, remove it
+            //and then make the game move faster
+
+            if (letterBox.Items.Contains(e.KeyCode))
+            {
+
+                letterBox.Items.Remove(e.KeyCode);
+                letterBox.Refresh();
+                if (timer.Interval > 400)
+                    timer.Interval = timer.Interval - 10;
+                if (timer.Interval > 250)
+                    timer.Interval = timer.Interval - 7;
+                if (timer.Interval > 100)
+                    timer.Interval = timer.Interval - 2;
+                progressBar.Value = 800 - timer.Interval;
+
+
+                //User pressed a correct key, so update the stats object
+                //by calling its update() method with argument true
+                stats.Update(true);
+
+            }
+            else
+            {
+                //The user pressed incorrect key, so update the stats object
+                //by calling its update() method with arguement false
+                stats.Update(false);
+            }
+
+            //Update status strip labels
+            correctLabel.Text = "Correct: " + stats.Correct;
+            missedLabel.Text = "Missed: " + stats.Missed;
+            accuracyLabel.Text = "Accuracry: " + stats.Accurate + "%";
+        }
+    }
+}
